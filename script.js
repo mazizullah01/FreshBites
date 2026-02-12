534f3347a8e479c4834a5c7d013fc824c574ea2d
mazizullah01 <m.azizullah420@gmail.com>
2026-02-12 22:56:05 +0500

    feat: styling hero section for small screen

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
  const menuOpenButton = document.querySelector("#menu-open-button");
+ const menuCloseButton = document.querySelector("#menu-close-button");
 
  menuOpenButton.addEventListener("click", () => {
     // Toggle moblie menu visibilty 
     document.body.classList.toggle("show-mobile-menu");
  });
+
+ // Close menu when the close button is clicked
+ menuCloseButton.addEventListener("click", () => menuOpenButton.click
+ ());
