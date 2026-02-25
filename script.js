8034ffdf21291854aef336e7ebce6d9918990620
mazizullah01 <m.azizullah420@gmail.com>
2026-02-25 16:12:45 +0500

    feat: Close menu when the nav link is clicked

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
- const menuOpenButton = document.querySelector("#menu-open-button");
- const menuCloseButton = document.querySelector("#menu-close-button");
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
 
  menuOpenButton.addEventListener("click", () => {
     // Toggle moblie menu visibilty 
@@ -9,6 +10,13 @@
  // Close menu when the close button is clicked
  menuCloseButton.addEventListener("click", () => menuOpenButton.click
  ());
+
+// Close menu when the nav link is clicked
+ navLinks.forEach(link => {
+    link.addEventListener("click" , () => menuOpenButton.click());
+ })
+
+
 
  // Intialize Swiper
   const swiper = new Swiper('.slider-wrapper', {
